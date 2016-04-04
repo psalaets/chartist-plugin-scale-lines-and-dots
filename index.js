da8f72eb3230b288aeff8cd8b01e8a24354d951e
Paul Salaets <psalaets@gmail.com>
2016-04-03 22:38:28 -0400

    function for creating scaled value

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 module.exports.calculateScaleFactor = calculateScaleFactor;
+module.exports.scaleValue = scaleValue;
 
 function calculateScaleFactor(min, max, value) {
   if (max <= min) {
@@ -12,4 +13,13 @@ function calculateScaleFactor(min, max, value) {
   scaleFactor = Math.max(scaleFactor, 0);
 
   return scaleFactor;
+}
+
+function scaleValue(min, max, scaleFactor) {
+  if (scaleFactor > 1) throw new Error('scaleFactor cannot be > 1');
+  if (scaleFactor < 0) throw new Error('scaleFactor cannot be < 0');
+  if (max < min) throw new Error('max cannot be < min');
+
+  var delta = max - min;
+  return (delta * scaleFactor) + min;
 }
---
+++ test/scale-value-test.js
@@ -0,0 +1,50 @@
+var test = require('tape');
+var scaleValue = require('../').scaleValue;
+
+test('returns min when scaleFactor is 0', function (t) {
+  t.plan(1);
+
+  var result = scaleValue(2, 6, 0);
+
+  t.equals(result, 2);
+});
+
+test('returns max when scaleFactor is 1', function (t) {
+  t.plan(1);
+
+  var result = scaleValue(2, 6, 1);
+
+  t.equals(result, 6);
+});
+
+test('handles scaleFactor between 0 and 1', function (t) {
+  t.plan(1);
+
+  var result = scaleValue(2, 6, 0.25);
+
+  t.equals(result, 3);
+});
+
+test('throws Error when scaleFactor is less than 0', function (t) {
+  t.plan(1);
+
+  t.throws(function () {
+    scaleValue(1, 2, -1);
+  });
+});
+
+test('throws Error when scaleFactor is greater than 1', function (t) {
+  t.plan(1);
+
+  t.throws(function () {
+    scaleValue(1, 2, 1.1);
+  });
+});
+
+test('throws Error when max is less than min', function (t) {
+  t.plan(1);
+
+  t.throws(function () {
+    scaleValue(2, 1, 0);
+  });
+});
