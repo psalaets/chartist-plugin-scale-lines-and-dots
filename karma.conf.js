7d6740819ebb2d3fe97ecea00abd5d384e8f5387
Paul Salaets <psalaets@gmail.com>
2016-05-08 09:35:54 -0400

    rename test dir

--- karma.conf.js
+++ karma.conf.js
@@ -21,7 +21,7 @@ module.exports = function(config) {
 
     // list of files / patterns to load in the browser
     files: [
-      'test/functional/*.js'
+      'test/browser/*.js'
     ],
 
 
@@ -33,7 +33,7 @@ module.exports = function(config) {
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
-      'test/functional/*.js': ['webpack']
+      'test/browser/*.js': ['webpack']
     },
 
     webpack: {
