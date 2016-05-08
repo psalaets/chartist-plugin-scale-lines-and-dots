3ec62aee74a992c023d49e48fe460a0b5eb8b93e
Paul Salaets <psalaets@gmail.com>
2016-05-08 00:03:49 -0400

    cache svg width so it's only read once per chart, not once per line/dot

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ makePluginInstance.scaleValue = scaleValue;
 
 function makePluginInstance() {
   return function scaleLinesAndDotsInstance(chart) {
+    var svgWidth;
+
     chart.on('draw', function(data) {
       if (data.type === 'point') {
         scaleDot(data);
@@ -12,27 +14,29 @@ function makePluginInstance() {
         scaleLine(data);
       }
     });
+
+    function scaleLine(data) {
+      scaleElementStrokeWidth(data, scaleValue.bind(null, 2, 6));
+    }
+
+    function scaleDot(data) {
+      scaleElementStrokeWidth(data, scaleValue.bind(null, 8, 16));
+    }
+
+    function scaleElementStrokeWidth(data, scaleFn) {
+      var scaleFactor = calculateScaleFactor(360, 1000, getSvgWidth(data));
+      var scaledStrokeWidth = scaleFn(scaleFactor);
+      var element = data.element;
+
+      element.attr({
+        style: 'stroke-width: ' + scaledStrokeWidth
+      });
+    }
+
+    function getSvgWidth(data) {
+      return svgWidth = svgWidth || data.element.root().width();
+    }
   };
-}
-
-function scaleLine(data) {
-  scaleElementStrokeWidth(data, scaleValue.bind(null, 2, 6));
-}
-
-function scaleDot(data) {
-  scaleElementStrokeWidth(data, scaleValue.bind(null, 8, 16));
-}
-
-function scaleElementStrokeWidth(data, scaleFn) {
-  var element = data.element;
-  var svgWidth = element.root().width();
-
-  var scaleFactor = calculateScaleFactor(360, 1000, svgWidth);
-  var scaledStrokeWidth = scaleFn(scaleFactor);
-
-  element.attr({
-    style: 'stroke-width: ' + scaledStrokeWidth
-  });
 }
 
 function calculateScaleFactor(min, max, value) {
